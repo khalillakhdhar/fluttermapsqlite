be7733708844a3cc48f116307d5d988d7f581630
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-03-26 19:35:35 +0100

    Update databaseHelper.dart

--- lib/data/databaseHelper.dart
+++ lib/data/databaseHelper.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'dart:async';
+import 'package:fluttermap/models/user.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
@@ -26,11 +27,35 @@ class DatabaseHelper {
     Directory documentDirectory = await getApplicationDocumentsDirectory();
     String path = join(documentDirectory.path, "main.db");
     var ourDb = await openDatabase(path, version: 1, onCreate: _onCreate);
+    return ourDb;
   }
 
   void _onCreate(Database db, int version) async {
     await db.execute(
         "CREATE TABLE User (id INTEGER PRIMARY KEY, name TEXT, username TEXT, password TEXT, flaglogged TEXT)");
     print("db created");
+  }
+
+  //insert
+  Future<int> saveUser(User user) async {
+    var dbClient = await db;
+    int res = await dbClient.insert("User", user.toMap());
+    print(" état de sauvegarde " + res.toString());
+    return res;
+  }
+
+  Future<User> selectUser(User user) async {
+    print(" authentification");
+    var dbClient = await db;
+
+    List<Map> maps = await dbClient.query(tableUser,
+        columns: [columnUsername, columnPassword],
+        where: "$columnUsername= ? and $columnPassword=?",
+        whereArgs: [user.username, user.password]);
+    if (maps.length > 0) {
+      print("exist!");
+      return user;
+    } else
+      return null;
   }
 }
